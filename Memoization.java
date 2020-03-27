281ce61109a463ae659a099f6a83f3e323098a85
Amrinder <amrinder.singh@outlook.de>
2020-03-27 16:45:01 -0700

    Added non memoizated solution

--- Memoization.java
+++ Memoization.java
@@ -8,11 +8,20 @@ public class Memoization {
     static HashMap<Integer, Integer> cache = new HashMap<>();
 
     public static void main(String[] args) {
-        int result = fib(25);
+        int result = fibWithCache(42);
         System.out.println(result);
     }
 
     private static int fib(int N) {
+        int result;
+        if (N < 2) {
+            result = N;
+        } else {
+            result = fib(N-1) + fib(N-2);
+        }
+        return result;
+    }
+    private static int fibWithCache(int N) {
         if (cache.containsKey(N)) {
             return cache.get(N);
         }
